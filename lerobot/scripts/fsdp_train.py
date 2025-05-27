a0225f66e0114ff5060b8586804de6292e5e3a73
xjh0803 <1259007219@qq.com>
2025-05-27 12:42:37 +0800

    HYBRID_SHARD in ShardingStrategy

--- lerobot/scripts/fsdp_train.py
+++ lerobot/scripts/fsdp_train.py
@@ -154,24 +154,24 @@ def train_step(model, batch, scaler, cfg):
 @parser.wrap()
 def train(cfg: TrainPipelineConfig):
     # 初始化分布式环境
-    # world_size = int(os.environ["WORLD_SIZE"])
-    # local_rank = int(os.environ["LOCAL_RANK"])
-    # world_rank = int(os.environ["RANK"])
-    # node_rank = int(os.environ["NODE_RANK"])
-    # master_ip = os.environ["MASTER_ADDR"]
-    # master_port = os.environ["MASTER_PORT"]
-    # master_uri = "tcp://%s:%s" % (master_ip, master_port)
-    # dist.init_process_group(
-    #     backend="nccl",
-    #     init_method=master_uri,
-    #     world_size=world_size,
-    #     timeout=timedelta(minutes=60),
-    #     rank=world_rank,
-    # )
-    dist.init_process_group(backend="nccl")
-    world_size = dist.get_world_size()
+    world_size = int(os.environ["WORLD_SIZE"])
+    local_rank = int(os.environ["LOCAL_RANK"])
+    world_rank = int(os.environ["RANK"])
+    node_rank = int(os.environ["NODE_RANK"])
+    master_ip = os.environ["MASTER_ADDR"]
+    master_port = os.environ["MASTER_PORT"]
+    master_uri = "tcp://%s:%s" % (master_ip, master_port)
+    dist.init_process_group(
+        backend="nccl",
+        init_method=master_uri,
+        world_size=world_size,
+        timeout=timedelta(minutes=60),
+        rank=world_rank,
+    )
+    # dist.init_process_group(backend="nccl")
+    # world_size = dist.get_world_size()
     rank = dist.get_rank()
-    local_rank = rank
+    # local_rank = rank
     # local_rank = int(os.environ["OMPI_COMM_WORLD_LOCAL_RANK"])
     # local_rank = node_rank
     torch.cuda.set_device(local_rank)
@@ -179,7 +179,7 @@ def train(cfg: TrainPipelineConfig):
     # 初始化配置
     cfg.validate()
     logger = init_logger(cfg, rank)
-    # logger.info(f"DIST INFO: world_size={world_size}, local_rank={local_rank}, world_rank={world_rank}, node_rank={node_rank}, master_uri={master_uri}")
+    logger.info(f"DIST INFO: world_size={world_size}, local_rank={local_rank}, world_rank={world_rank}, node_rank={node_rank}, master_uri={master_uri}")
     
     if int(os.environ.get('RANK', 0)) == 0:
         logger.info(pformat(cfg.to_dict()))
