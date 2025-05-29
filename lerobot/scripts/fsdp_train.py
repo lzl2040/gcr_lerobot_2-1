79f83e4c4ef31253747a32564898801ba0172762
lzlglory <lizuolei24@ime.ac.cn>
2025-05-29 10:51:09 +0000

    remove image aug for simpler fractal

--- lerobot/scripts/fsdp_train.py
+++ lerobot/scripts/fsdp_train.py
@@ -210,7 +210,8 @@ def train(cfg: TrainPipelineConfig):
             step = sorted(steps)[-1] + 1
             seed += (step-1)
             
-    image_transforms = (ImageTransforms(cfg.dataset.image_transforms))
+    # image_transforms = (ImageTransforms(cfg.dataset.image_transforms))
+    image_transforms = None
     print(image_transforms)
     dataset = MultiDatasetforDistTraining(
         cfg=cfg, 
