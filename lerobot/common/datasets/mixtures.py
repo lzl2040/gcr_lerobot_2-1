7f3844b7f51aa3fe99fa23893924e79dbadcc4af
lzlglory <lizuolei24@ime.ac.cn>
2025-05-29 08:09:34 +0000

    update libero_long_frame_75

--- lerobot/common/datasets/mixtures.py
+++ lerobot/common/datasets/mixtures.py
@@ -233,6 +233,9 @@ OXE_NAMED_MIXTURES: Dict[str, List[Tuple[str, float]]] = {
          ("libero_spatial_no_noops", 1.0), 
          ("libero_10_no_noops", 1.0),
      ],
+     "libero_long": [
+         ("libero_10_no_noops", 1.0),
+     ],
      "simpler_fractal":[
          ("simpler_fractal", 1.0), 
      ],
